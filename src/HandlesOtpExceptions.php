1c886f019f61046a4dffa3f330243bfe78a99ae0
Artem Prozorov <artem.g.prozorov@gmail.com>
2020-12-12 00:26:22 +0300

    Added exception handler

---
+++ src/HandlesOtpExceptions.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zebrains\LaravelDataLocker;
+
+use Prozorov\DataVerification\Exceptions\VerificationException;
+use Prozorov\DataVerification\Exceptions\LimitException;
+
+class HandlesOtpExceptions
+{
+    /**
+     * Register a renderable callback.
+     *
+     * @param  callable  $renderUsing
+     * @return $this
+     */
+    abstract public function renderable(callable $renderUsing);
+
+    /**
+     * Registers errorhandlers for OTP exceptions
+     *
+     * @access	protected
+     * @return	void
+     */
+    protected function registerOtpExceptionHandlers(): void
+    {
+        $this->renderable(function (VerificationException $exception, $request) {
+            return response()->json([
+                'code' => 400,
+                'message' => $exception->getMessage(),
+            ], 400);
+        });
+
+        $this->renderable(function (LimitException $exception, $request) {
+            return response()->json([
+                'code' => 400,
+                'message' => $exception->getMessage(),
+            ], 400);
+        });
+    }
+}
